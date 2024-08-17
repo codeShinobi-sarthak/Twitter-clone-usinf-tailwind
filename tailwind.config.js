bce39809a45170330cd62c8dc006e0c0f895acf2
codeShinobi-sarthak <codeshinobi.sarthak@gmail.com>
2024-08-17 08:48:21 +0530

    Add files via upload

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./*.html"],
+  theme: {
+    extend: {
+      colors: {
+        'dark-color': '#16181c',
+        'blue-btn': '#1a8cd8',
+      },
+    },
+  },
+  plugins: [],
+}
+
